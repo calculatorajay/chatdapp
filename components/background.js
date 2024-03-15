e3c324580e45d61c8cb0cdab40aeaa754c10548e
ajaykumarrediff <calculatorajay@gmail.com>
2024-03-16 00:28:35 +0530

    second

---
+++ components/background.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Background() {
+  return (
+    <div className="min-w-full">
+      <div className="w-full h-96 bg-cyan-700 dark:bg-gray-800"></div>
+    </div>
+  );
+}
